26685d4acb42e1152400ec51fa497a7aa8b3c7f3
Damien Vassart <damienvassart@tutanota.com>
2020-03-06 14:15:26 +0100

    added comments to the script.js file and corrected typos in index.html

--- holidayChecker/script.js
+++ holidayChecker/script.js
@@ -17,26 +17,31 @@ var endDate = document.getElementById("end-date");
 var endTime = document.getElementById("end-time");
 var result = document.getElementById("result");
 
+// adds a '0' before the number of minutes & the number of seconds if it is less than 10:
 function twoDigits(n) {
     return n < 10 ? "0" + n : n;
 }
 
+// adds a 's' the the words 'day', 'hour', 'minute' & 'second' if they come after a number bigger than 1:
 function pad(n, str) {
     if (n < 1) return "";
     else if (n === 1) return n + str + " ";
     else if (n > 1) return n + str + "s ";
 }
 
+// returns the interval between the dates sent by the user:
 function interval(a, b) {
     var d = Math.floor((a - b) / 86400000);
     var h = Math.floor((a - b) / 3600000) % 24;
     var m = Math.floor((a - b) / 60000) % 60;
     var s = Math.floor((a - b) / 1000) % 60;
-    return pad(d, " day") + pad(h, " hour") + pad(twoDigits(m), " minute") + pad(twoDigits(s), " second");
-}
+    return pad(d, " day") + pad(h, " hour") + pad(twoDigits(m), " minute") + pad(twoDigits(s), " second") + "</p>";
+};
 
+// Time input fields being optional, a default value is set to 00:00 (12:00 AM)
 startTime.defaultValue = "00:00";
 endTime.defaultValue = "00:00";
+// before the user sends input, the div containing the result is hidden:
 result.style.visibility = "hidden";
 
 form.addEventListener("submit", function(e) {
@@ -46,10 +51,10 @@ form.addEventListener("submit", function(e) {
     // These variables will store the user's input:
     var targetStartDate = new Date(startDate.value);
     var targetEndDate = new Date(endDate.value);
-    var targetStartTime = startTime.value.split("");
-    var targetEndTime = endTime.value.split("");
+    var targetStartTime = startTime.value.split(""); // time input field sends data as a string; we split it to separate the hours and minutes and store them in an array
+    var targetEndTime = endTime.value.split(""); // same than above
 
-    // Setting the time for the given dates:
+    // Setting the hours and minutes for the given dates:
     targetStartDate.setHours(targetStartTime[0]);
     targetStartDate.setMinutes(targetStartTime[1]);
     targetStartDate.setSeconds(0);
@@ -58,22 +63,21 @@ form.addEventListener("submit", function(e) {
     targetEndDate.setSeconds(0);
 
     // Displaying the result:
-    var answer = document.createElement("p");
 
     if (targetStartDate > targetEndDate) {
-        answer.innerHTML =
-            "Your vacation <strong>cannot</strong> start after it ends !";
+        result.innerHTML =
+            "<p>Your vacation <strong>cannot</strong> start after it ends !</p>";
     } else {
         if (now < targetStartDate) {
-            answer.innerHTML = "Hold tight, there's still <br>" + interval(targetStartDate, now) + " to go before your vacation !";
+            result.innerHTML = "<p>" + interval(targetStartDate, now) + " remaining.</p>";
         } else if (now > targetStartDate && now < targetEndDate) {
-            answer.innerHTML = "Booom ! You are on vacation and you have <br>" + interval(targetEndDate, now) + " left. Enjoy !";
+            result.innerHTML = "<p>" + interval(targetEndDate, now) + "</p><p> of vacation left.</p>";
         } else {
-            answer.innerHTML =
-                "Oh, noes ! Seems like your vacation ended <br>" + interval(now, targetEndDate) + " ago. Wait for the next one !";
+            result.innerHTML = "<p>Vacation ended </p><p>";
+            result.innerHTML += interval(now, targetEndDate);
+            result.innerHTML += "</p><p> ago.</p>";
         }
     }
 
-    result.appendChild(answer);
     e.preventDefault();
 });
