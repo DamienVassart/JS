ff4abbc2c3dce67b841d5cd6006bc3d1d926f327
Damien Vassart <damienvassart@tutanota.com>
2020-10-19 17:42:17 +0200

    finished to code App.jsx and TodoItem.jsx

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+import todosData from './todosData';
+import TodoItem from './TodoItem';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+  render() {
+    const todoList = this.state.todos.map(todo => <TodoItem key={todo.id} todo={todo} handleChange={this.handleChange} />)
+    return (
+      <div>
+        {todoList}
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ todo-list/src/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox"
+                checked={props.todo.completed}
+                onChange={() => props.handleChange(props.todo.id)} 
+            />
+            <p>{props.todo.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
